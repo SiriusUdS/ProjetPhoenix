49bf373d6f5a786d6c930ee1c55eae275bc1a62e
St-Amand,Olivier <stao0901@usherbrooke.ca>
2024-02-05 17:56:38 -0500

    Replace googletest by unity

---
+++ cmake/unity.cmake
@@ -0,0 +1,8 @@
+include(FetchContent)
+
+FetchContent_Declare(
+        Unity
+        URL https://github.com/ThrowTheSwitch/Unity/archive/refs/tags/v2.6.0.zip
+)
+
+FetchContent_MakeAvailable(unity)
