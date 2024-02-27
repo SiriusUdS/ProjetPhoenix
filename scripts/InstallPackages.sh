3d310b7899fc7fd9873e64bf7a0ae6ce12df626d
St-Amand,Olivier <stao0901@usherbrooke.ca>
2024-02-26 20:58:30 -0500

    Added readme and script to install apt packages

---
+++ scripts/InstallPackages.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+sudo apt update
+sudo apt install -y                                      \
+ ack                                                     \
+ apt-utils                                               \
+ build-essential                                         \
+ ca-certificates                                         \
+ curl                                                    \
+ git                                                     \
+ htop                                                    \
+ locate                                                  \
+ make                                                    \
+ openocd                                                 \
+ nano                                                    \
+ openssh-client                                          \
+ wget                                                    \
+ cmake                                                   \
+ gcc-arm-none-eabi gdb-arm-none-eabi                     \
+ protobuf-compiler                                       \
+ python3                                                 \
+ python3-pip                                             \
+ nanopb
